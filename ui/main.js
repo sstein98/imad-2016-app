7efd06d9718a73f69b7f928468eabf715adb469e
ashikameer <ashikameer100@gmail.com>
2016-12-04 15:05:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,16 +7,18 @@ function moveRight(){
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
 };
+button.onclick=function(){
 
-var req=XMLHttpRequest();
-req.onreadystatechange=function(){
-    if (req.onreadystatechange===XMLHttpRequest.DONE){
-        if(req.status===200){
-            var counter=req.responseText; 
-            var span=document.getElementById("count");
-            span.innerHTML=counter.toString();
+    var req=XMLHttpRequest();
+    req.onreadystatechange=function(){
+        if (req.onreadystatechange===XMLHttpRequest.DONE){
+            if(req.status===200){
+                var counter=req.responseText; 
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }
         }
-    }
+    };
     req.open('GET','http://ashikameer.imad.hasura-app.io/counter',true);
     req.send(null);
 };
